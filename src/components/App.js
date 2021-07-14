0e7d73d91e46c2cb42da3ce3fa1d79f9425edfdb
Laarni Cortez <RainZetroc@hotmail.com>
2021-07-14 22:14:17 +0800

    add display flex and justify content in mobile view

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
+        <div className="ui long history  grid">
           <div className=" ui stackable grid row">
             <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
